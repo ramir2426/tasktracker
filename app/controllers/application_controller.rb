52e5deed7e929e0fd8786d2eb3db3c265582aa7a
rana-amir@bayt.net <rana-amir@bayt.net>
2021-02-14 17:20:16 +0100

    add comments and generate PR to CI/CD

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 # Application Controller
 class ApplicationController < ActionController::Base
+  #Befor actions
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
+  #devise strong params
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: %i[name email role team_id])
@@ -15,6 +16,7 @@ class ApplicationController < ActionController::Base
 
   private
 
+  #intialization for user profile picture
   def init_attachment
     current_user.avatar.attach(params[:avatar]) if current_user.update
   end
